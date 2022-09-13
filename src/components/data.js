c999db52a80afaff3c602ffb7b23f0f101e5efa9
hacks-and-codes <owusuansahkwadwo24@gmail.com>
2022-09-13 09:21:51 +0000

    Styling my work

---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-dupe-keys */
+// eslint-disable-next-line import/no-anonymous-default-export
+export default[
+    {
+    id: 1,
+    title: "Amazon Forest",
+    location: "",
+    description: "",
+    img: './images/14.-Amazon-1.jpg',
+    googleMap: '',
+    date:{
+        startDate: '18th November 2019',
+        endDate: '27th December, 2019'},
+    id: 2,
+    title: "Atwea Mountains",
+    location: "Ghana",
+    description: "",
+    img: './images/atwea.jpg',
+    googleMap: '',
+    date:{
+        startDate: '24th June 2017',
+        endDate: '30th June 2017'},
+    id: 2,
+    title: "Cape Coast Castle",
+    location: "Ghana",
+    description: "",
+    img: './images/castle.jpg',
+    googleMap: '',
+    date:{
+        startDate: '20th March, 2021',
+        endDate: '20th March,'},
+}]
---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <nav>
+<h1 className='heading'>Travel Journal</h1>
+    <div className='navbar--container'>
+    <h5 className='navbar-text'>my travel journal.</h5>
+    </div>
+    </nav>
+  )
+}
+
+export default navbar
